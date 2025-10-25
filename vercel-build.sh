a2b2120329aae77ce95b5b339a5e8164f4a4ce98
Htet Paing Linn <htetpainglinn2822004@gmail.com>
2025-10-26 03:01:33 +0630

    to railway

--- vercel-build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Install Composer dependencies
-composer install --no-dev --optimize-autoloader --no-interaction
-
-# Clear and cache Laravel config
-php artisan config:cache
-php artisan route:cache
-php artisan view:cache
